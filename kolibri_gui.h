cd962d78f807afaab69bb868ea1e0e40180ebd57
Ashish Gupta <ashmew2@gmail.com>
2015-09-12 03:40:15 +0530

    Files synced with GUI API PROPER!

--- kolibri_gui.h
+++ kolibri_gui.h
@@ -1,86 +1,27 @@
 #ifndef KOLIBRI_GUI_H
 #define KOLIBRI_GUI_H
 
-#include <kos32sys.h>
-#include "kolibri_debug.h"
 #include "stdlib.h" /* for malloc() */
+#include <kos32sys.h>
+
+#include "kolibri_debug.h" /* work with debug board */
 
 /* boxlib loader */
 #include "kolibri_boxlib.h"
 
-/* GUI Elements being used */
-#include "kolibri_editbox.h"
-#include "kolibri_checkbox.h"
+/* All supported GUI elements included */
+#include "kolibri_gui_elements.h"
 
-/* KOLIBRI_GUI_ELEMENT_TYPE contains all available GUI items from box_lib */
-/* More elements can be added from other libraries as required */
-
-enum KOLIBRI_GUI_ELEMENT_TYPE {
-  KOLIBRI_EDIT_BOX,
-  KOLIBRI_CHECK_BOX,
-  KOLIBRI_RADIO_BUTTON,
-  KOLIBRI_SCROLL_BAR,
-  KOLIBRI_DYNAMIC_BUTTON,
-  KOLIBRI_MENU_BAR,
-  KOLIBRI_FILE_BROWSER,
-  KOLIBRI_TREE_LIST,
-  KOLIBRI_PATH_SHOW,
-  KOLIBRI_TEXT_EDITOR,
-  KOLIBRI_FRAME,
-  KOLIBRI_PROGRESS_BAR,
-
-  /* Add elements above this element in order to let KOLIBRI_NUM_GUI_ELEMENTS */
-  /* stay at correct value */
-  
-  KOLIBRI_NUM_GUI_ELEMENTS 
-};
-
-/* Generic structure for supporting functions on various elements of Kolibri's GUI */
-struct kolibri_element_operations {
-  void (*redraw_fn)(void *);
-  void (*mouse_fn)(void *);
-  void (*key_fn)(void *);
-};
-
-/* Define operations for various GUI elements in one place */
-struct kolibri_element_operations kolibri_gui_op_table[KOLIBRI_NUM_GUI_ELEMENTS];
-
-void kolibri_init_gui_op_table(void)
-{
-/* Setting up functions for edit box GUI element */
-kolibri_gui_op_table[KOLIBRI_EDIT_BOX].redraw_fn = edit_box_draw;
-kolibri_gui_op_table[KOLIBRI_EDIT_BOX].mouse_fn = edit_box_mouse;
-kolibri_gui_op_table[KOLIBRI_EDIT_BOX].key_fn = editbox_key;
-
-kolibri_gui_op_table[KOLIBRI_CHECK_BOX].redraw_fn = check_box_draw2;
-kolibri_gui_op_table[KOLIBRI_CHECK_BOX].mouse_fn = check_box_mouse2;
-kolibri_gui_op_table[KOLIBRI_CHECK_BOX].key_fn = NULL;
-}
-
-/* This is a doubly linked list of all elements that need to be drawn in order */
-struct kolibri_window_element {
-  enum KOLIBRI_GUI_ELEMENT_TYPE type;
-  void *element;
-  struct kolibri_window_element *next, *prev;
-};
-
-struct kolibri_window {
-  unsigned int topleftx, toplefty;
-  unsigned int sizex, sizey;
-  char *window_title;
-
-  /* Refer to sysfuncs, value to be stored in EDX (Function 0) */
-  unsigned int XY;
-
-  struct kolibri_window_element *elements;
+enum KOLIBRI_GUI_EVENTS {
+  KOLIBRI_EVENT_REDRAW = 1,   /* Window and window elements should be redrawn */
+  KOLIBRI_EVENT_KEY = 2,      /* A key on the keyboard was pressed */
+  KOLIBRI_EVENT_BUTTON = 3,   /* A button was clicked with the mouse */
+  KOLIBRI_EVENT_MOUSE = 6     /* Mouse activity (movement, button press) was detected */
 };
 
 void kolibri_handle_event_redraw(struct kolibri_window* some_window)
 {
   /*  Draw windows with system color table. */
-  /* Replace this with inline assembly? */
-
-  //  init_boxlib_asm();
 
   BeginDraw();
 
@@ -152,59 +93,13 @@ void kolibri_handle_event_mouse(struct kolibri_window* some_window)
     }
 }
 
-struct kolibri_window * kolibri_new_window(int tlx, int tly, int sizex, int sizey, char *title)
-{
-  struct kolibri_window *new_win = (struct kolibri_window *)malloc(sizeof(struct kolibri_window));
-
-  new_win->topleftx = tlx;
-  new_win->toplefty = tly;
-  new_win->sizex = sizex;
-  new_win->sizey = sizey;
-  new_win->window_title = title;
-  new_win->XY = 0x00000013; /* All windows are skinned windows with caption for now */
-  new_win->elements = NULL;
-  
-  return new_win;
-}
-
-/* Add an element to an existing window */
-void kolibri_window_add_element(struct kolibri_window *some_window, enum KOLIBRI_GUI_ELEMENT_TYPE element_type, void *some_gui_element)
-{
-  struct kolibri_window_element *new_element = (struct kolibri_window_element *)malloc(sizeof(struct kolibri_window_element));
-  
-  new_element -> type = element_type;
-  new_element -> element = some_gui_element;
-
-  if(!(some_window->elements)) /* No elements in window yet */
-    {
-      some_window->elements = new_element;
-      some_window->elements -> prev = some_window->elements;
-      some_window->elements -> next = some_window->elements;
-    }
-  else
-    {
-      struct kolibri_window_element *last_element = some_window -> elements -> prev;
-  
-      last_element -> next = new_element;
-      new_element -> next = some_window -> elements; /* start of linked list  */
-      some_window -> elements -> prev = new_element;
-      new_element -> prev = last_element;
-    }
-}
-
-enum KOLIBRI_GUI_EVENTS {
-  KOLIBRI_EVENT_REDRAW = 1,   /* Window and window elements should be redrawn */
-  KOLIBRI_EVENT_KEY = 2,      /* A key on the keyboard was pressed */
-  KOLIBRI_EVENT_BUTTON = 3,   /* A button was clicked with the mouse */
-  KOLIBRI_EVENT_MOUSE = 6     /* Mouse activity (movement, button press) was detected */
-};
-
 int kolibri_main_gui_loop(struct kolibri_window* main_window)
 {
   /* Set gui_event to REDRAW so that window is drawn in first iteration  */
   unsigned int gui_event = KOLIBRI_EVENT_REDRAW;
 
   int boxlib_init_status = kolibri_boxlib_init();
+
   if(boxlib_init_status == 0) 
     board_write_str("GUI Successfully Initialized.\n");
   else 
@@ -214,7 +109,7 @@ int kolibri_main_gui_loop(struct kolibri_window* main_window)
     }
 
   /* Initialize the global operation table which handles event functions of */
-  /*   each individual element type */
+  /* each individual element type */
   kolibri_init_gui_op_table();
 
   /* Get the current color table for Kolibri and store in global table*/
@@ -246,7 +141,7 @@ int kolibri_main_gui_loop(struct kolibri_window* main_window)
 
     } while(gui_event = get_os_event()); /* End of main activity loop */
   
-  //kolibri_quit(); //Free alloc'd stuff.
+  /* kolibri_quit(); */
 }
 
 /* Note: The current implementation tries to automatically colors 
--- kolwin.c
+++ kolwin.c
@@ -4,8 +4,7 @@ int main()
 {
   struct kolibri_window *main_window = kolibri_new_window(0, 0, 1024, 500, "New KolibriOS Window!");
   struct edit_box *textbox = kolibri_new_edit_box(100, 100, 10);
-  char *checkbox_label = "Free Sex";
-  struct check_box *checkbox = kolibri_new_check_box(200, 200, 16, 12, checkbox_label);
+  struct check_box *checkbox = kolibri_new_check_box(200, 200, 16, 12, "FREE SEX HERE!");
   
   kolibri_window_add_element(main_window, KOLIBRI_EDIT_BOX, textbox);
   kolibri_window_add_element(main_window, KOLIBRI_CHECK_BOX, checkbox);
